633737809cede4bdf2313b3896b18342a6fbbc9e
ear-123 <elias.a.rajala@gmail.com>
2024-01-22 21:50:02 +0200

    first commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+    
+        Hedgehog hedgehog = new Hedgehog();
+        boolean exit = false;
+
+        while (!exit) {
+
+            System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+            
+            Scanner scan = new Scanner(System.in);
+
+            String strininput = scan.nextLine();
+            int option = 0;
+            option = Integer.parseInt(strininput);
+
+            if (option == 0) {
+                System.out.println("Kiitos ohjelman käytöstä.");
+                exit = true;
+                continue;
+            }
+            if (option == 1) {
+                System.out.println("Mitä siili sanoo:");
+                hedgehog.speak(scan.nextLine());
+                continue;
+            }
+            if (option == 2) {
+                System.out.println("Anna siilin nimi:");
+                String name = scan.nextLine();
+                System.out.println("Anna siilin ikä:");
+                int age = Integer.parseInt(scan.nextLine());
+                hedgehog.newHedgehog(name, age);
+                continue;
+            }
+            if (option == 3) {
+                System.out.println("Kuinka monta kierrosta?");
+                int laps = Integer.parseInt(scan.nextLine());
+                hedgehog.run(laps);
+                continue;
+            }
+            else {
+                System.out.println("Syöte oli väärä");
+                continue;
+            }  
+        } 
+    }
+}
---
+++ src/main/java/main/Hedgehog.java
@@ -0,0 +1,41 @@
+package main;
+
+
+public class Hedgehog {
+    private String name;
+    private int age;
+
+    public Hedgehog() {
+        name = "Pikseli";
+        age = 5;
+    }
+
+    public void speak(String text) {
+        if(text == ""){
+            System.out.println("Olen " + this.name+ " ja ikäni on " + this.age + ", mutta antaisitko sili syötteen?");
+        }
+        else {
+            System.out.println(name + ": " + text);
+        }
+        
+    }
+
+    public void newHedgehog(String newName, int newAge) {
+        this.name = newName;
+        this.age = newAge;
+    }
+    
+    public void run(int laps) {
+        int i = 0;
+        boolean exit = false;
+        while (!exit) {
+            System.out.println(this.name + " juoksee kovaa vauhtia!");
+            i = i + 1;
+            if (i >= laps) {
+                exit = true;
+            }
+        
+        }
+    }
+}
+
